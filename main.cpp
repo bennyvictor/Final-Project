422e8373d7b39a8640c559c7f57c573bf48b56bc
Unknown <benny.victor8@gmail.com>
2016-10-21 08:20:05 +0700

    Add Via Gitkraken

--- main.cpp
+++ main.cpp
@@ -10,8 +10,13 @@ using namespace std;
 
 int main ()
 {
+	cout << "Welcome to Prime Factorizer" << endl;
+	cout << "Keep entering your numbers to prime factorizer" << endl;
+	cout << "or enter -1 to Quit" << endl;
 	
+	int num = 0;
 	
+	cout << "Enter the number :";
 	
 	
 	return 0;
