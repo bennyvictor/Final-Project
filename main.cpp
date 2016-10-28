1bbb63b7cd47983b2bca3bc68b61035ed21bb259
Unknown <benny.victor8@gmail.com>
2016-10-28 10:52:45 +0700

    Update

--- main.cpp
+++ main.cpp
@@ -5,8 +5,36 @@
 //Created by Benny Victor
 
 #include <iostream>
+#include <sstream>
+#include <vector>
 
 using namespace std;
+
+string toString (int a)
+{
+	ostringstream g;
+	g << a;
+	return g.str();
+}
+
+void printVector (vector<int> v)
+{
+	string output = "{";
+	for (int i; i<v.size(); i++)
+	{
+		ostringstream a;
+		a << v[i];
+		if (i==(v.size()-1))
+		{
+			output += a.str() + "}";
+		}
+		else 
+		{
+			output += a.str() + ", ":
+		}
+	}
+	cout << output << endl;
+}
 
 int main ()
 {
@@ -18,6 +46,15 @@ int main ()
 	
 	cout << "Enter the number :";
 	
+	while (num != -1)
+	{
+		if (num != 0)
+		{
+			cout << primeFactorize (num) << endl; 
+		}
+		cout << ">>>";
+		cin >> num;
+	}
 	
 	return 0;
 }
