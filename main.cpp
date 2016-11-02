a41cc9372405d86d7b91da086d00c71edb821bdf
Unknown <benny.victor8@gmail.com>
2016-11-02 09:59:04 +0700

    Update

--- main.cpp
+++ main.cpp
@@ -36,6 +36,46 @@ void printVector (vector<int> v)
 	cout << output << endl;
 }
 
+vector<int> Factors (int num)
+{
+	vector<int> f;
+	for (int i=1; i<=num; ++i)
+	{
+		if (num%i==0)
+		{
+			f.push_back(i);
+		}
+	}
+	return f;
+}
+
+bool isprime(int num)
+{
+	if (Factors(num).size()==2)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+vector<int> primeFactors(vector<int> f)
+{
+	vector<int> r;
+	for (int i=0; i<f.size(); ++i)
+	{
+		if (isprime(f[i]))
+		{
+			r.push_back(f[i]);
+		}
+	}
+	return r;
+}
+
+
+
 int main ()
 {
 	cout << "Welcome to Prime Factorizer" << endl;
